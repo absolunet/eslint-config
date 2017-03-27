fbc44546c316753ae150a95e55d3981fe7ab4262
Jean-Baptiste Landry <jblandry@absolunet.com>
2017-03-27 15:34:18 -0400

    Conversion à @absolunet/eslint-config-core

--- packages/node/index.js
+++ packages/node/index.js
@@ -3,9 +3,4 @@
 //--------------------------------------------------------
 'use strict';
 
-const fs   = require('fs');
-const yaml = require('js-yaml');
-
-module.exports = (() => {
-	return yaml.safeLoad(fs.readFileSync(`${__dirname}/.eslintrc.yaml`, 'utf8'));
-})();
+module.exports = require('@absolunet/eslint-loader')(__dirname);
