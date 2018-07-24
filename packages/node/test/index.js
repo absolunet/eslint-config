df50c26958da335496152c83bc20c1f065a0a72c
Jean-Baptiste Landry <jblandry@absolunet.com>
2018-07-24 14:21:44 -0400

    Cleanup + Better tester

--- packages/node/test/index.js
+++ packages/node/test/index.js
@@ -3,20 +3,6 @@
 //--------------------------------------------------------
 'use strict';
 
-const ava    = require('ava');
 const tester = require('@absolunet/tester');
 
-
-
-
-
-
-tester.lintJs();
-tester.lintJson();
-tester.lintYaml();
-
-ava.test('Config loads', (t) => {
-	const data = require('../index');  // eslint-disable-line global-require
-	t.deepEqual(typeof data, 'object', 'YAML is not parsable');
-	t.deepEqual(typeof data.rules, 'object', 'Does not contain rules');
-});
+tester.npmPackage.validateEslintConfig();
