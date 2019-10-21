2948bf552592f9e0e6572af2002170fab9e88e99
Jean-Baptiste Landry <jblandry@absolunet.com>
2019-10-21 15:36:42 -0400

    Update tester

--- test/feature/index.test.js
+++ test/feature/index.test.js
@@ -10,30 +10,34 @@ const { CLIEngine } = require(require.resolve('eslint', { paths: [`${__dirname}/
 //-- Validate that subpackages return config
 Object.entries(tester.subpackages).forEach(([name, root]) => {
 	describe(`Validate that '${name}' works`, () => {
-		test(`Ensure package returns valid ESlint shareable config`, () => {
+		let config;
+		let eslint;
 
-			let config;
-
+		test(`Ensure YAML is parsable`, () => {
 			expect(() => {
 				config = require(root);  // eslint-disable-line global-require
-			}, 'YAML is not parsable').not.toThrow();
+			}).not.toThrow();
+		});
 
-			expect(config, 'Must contain config').toBeObject().not.toEqual({});
+		test(`Ensure file contains config`, () => {
+			expect(config).toBeObject();
+			expect(config).not.toEqual({});
+		});
 
-			let eslint;
-
+		test(`Ensure config is parsable by ESLint`, () => {
 			expect(() => {
 				eslint = new CLIEngine({
 					baseConfig: config,
 					cwd: root,
 					useEslintrc: false
 				});
-			}, 'Config is not parsable by ESLint').not.toThrow();
-
-			expect(() => {
-				eslint.executeOnText('');
-			}, 'Config is not valid').not.toThrow();
+			}).not.toThrow();
 		});
 
+		test(`Ensure config can be used`, () => {
+			expect(() => {
+				eslint.executeOnText('');
+			}).not.toThrow();
+		});
 	});
 });
--- test/standards/index.test.js
+++ test/standards/index.test.js
@@ -10,10 +10,14 @@ Object.values(tester.subpackages).forEach((root) => {
 	const configPath = `${root}/package.json`;
 
 	describe(`Validate ${tester.getReadablePath(configPath)}`, () => {
-		test(`Ensure 'package.json' contains ESLint shareable config metadata`, () => {
-			const config = fss.readJson(configPath);
-			expect(config.name, 'Name must be valid').toMatch(/^@absolunet\/eslint-config-(?<kebab1>[a-z][a-z0-9]*)(?<kebab2>-[a-z0-9]+)*$/u);
-			expect(config.keywords, 'Keywords must be valid').toIncludeAllMembers(['eslint', 'eslintconfig', 'eslint-config']);
+		const config = fss.readJson(configPath);
+
+		test(`Ensure name is valid`, () => {
+			expect(config.name).toMatch(/^@absolunet\/eslint-config-(?<kebab1>[a-z][a-z0-9]*)(?<kebab2>-[a-z0-9]+)*$/u);
+		});
+
+		test(`Ensure keywords are valid`, () => {
+			expect(config.keywords).toIncludeAllMembers(['eslint', 'eslintconfig', 'eslint-config']);
 		});
 	});
 });
