8e519f90ea776848cd7b4eecdeb7fee81893ff81
Jean-Baptiste Landry <jblandry@absolunet.com>
2021-01-25 16:22:19 -0500

    Corrections tester

--- test/generic/matrix/file-matrix.js
+++ test/generic/matrix/file-matrix.js
@@ -1,5 +1,7 @@
+import path from 'path';
+
 export default {
-	'.eslintrc.yaml':      `${__dirname}/root-eslintrc.yaml`,
-	'src/.eslintrc.yaml':  `${__dirname}/src-eslintrc.yaml`,
-	'test/.eslintrc.yaml': `${__dirname}/test-eslintrc.yaml`
+	'.eslintrc.yaml':      path.join(__dirname, 'root-eslintrc.yaml'),
+	'src/.eslintrc.yaml':  path.join(__dirname, 'src-eslintrc.yaml'),
+	'test/.eslintrc.yaml': path.join(__dirname, 'test-eslintrc.yaml')
 };
